74cc8b9f44cdd58281a6b8d54d8b8b82adb5f048
Samir Mirzaliyev <samir.mirzaliyev@norwexic.com>
2024-08-29 20:54:25 +0400

    Updated

--- babel.config.ts
+++ babel.config.ts
@@ -1,9 +1,7 @@
-const config = {
+module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-react",
     "@babel/preset-typescript",
   ],
 };
-
-export default config;
