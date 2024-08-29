a8243938fae1951ff4ee95ff2ec2b88bb96245de
Samir Mirzaliyev <samir.mirzaliyev@norwexic.com>
2024-08-29 14:45:56 +0400

    added jest and babel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-
+import React from "react";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <p className="m-0 max-w-[30ch] text-sm opacity-50">
-         hello
-        </p>
+        <h4 className="m-0 max-w-[30ch] text-sm opacity-50">hello</h4>
       </div>
     </main>
   );
