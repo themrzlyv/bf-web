a0edefc6c5468bdf607f218ea859fb5331307138
Samir Mirzaliyev <samir.mirzaliyev@norwexic.com>
2024-08-29 21:15:00 +0400

    added new text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <h4 className="m-0 max-w-[30ch] text-sm opacity-50">hello</h4>
       </div>
+      <p className="text-red-500">guys im here</p>
     </main>
   );
 }
