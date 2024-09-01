0a80c05ab1b474689c3456af8be17151f11dde88
Samir <72567167+themrzlyv@users.noreply.github.com>
2024-09-01 10:30:45 +0400

    Develop (#8)
    * added new text (#1)
    
    Co-authored-by: Samir Mirzaliyev <samir.mirzaliyev@norwexic.com>
    
    * changed bg color (#4)
    
    Co-authored-by: Samir Mirzaliyev <samir.mirzaliyev@norwexic.com>
    
    * added home structure (#6)
    
    * Updated tailwind config for files
    
    ---------
    
    Co-authored-by: Samir Mirzaliyev <samir.mirzaliyev@norwexic.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
 
     "./modules/**/**/*.{js,ts,jsx,tsx,mdx}",
     "./modules/**/*.{js,ts,jsx,tsx,mdx}",
+
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -30,7 +33,7 @@ const config: Config = {
         bold: "700",
         default: "400",
         thin: "300",
-        medium: "500"
+        medium: "500",
       },
     },
   },
